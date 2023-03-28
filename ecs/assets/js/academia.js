b419ef292f42b022078cb7c989265c55a0f8116c
yo-sarawut <yo_sarawut@hotmail.com>
2023-03-28 15:22:19 +0700

    .

--- ecs/assets/js/academia.js
+++ ecs/assets/js/academia.js
@@ -81,6 +81,24 @@
     }
   });
 
+  // Collapsible
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+
+
   // easeInOutExpo Declaration
   jQuery.extend(jQuery.easing, {
     easeInOutExpo: function (x, t, b, c, d) {
